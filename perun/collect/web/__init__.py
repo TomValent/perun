c0395accff927d619816de2e04f3e59a455bc898
Tomas Valent <xvalen27@fit.vutbr.cz>
2024-03-01 16:49:03 +0100

    web collector init

---
+++ perun/collect/web/__init__.py
@@ -0,0 +1,14 @@
+"""Trace collector collects running times of C/C++ functions.
+The collected data are suitable for further postprocessing using
+the regression analysis and visualization by scatter plots.
+"""
+
+COLLECTOR_TYPE = "mixed"
+COLLECTOR_DEFAULT_UNITS = {
+    "page_requests":           "",
+    "error_count":             "",
+    "error_code_count":        "",
+    "error_message_count":     "",
+    "memory_usage_counter":    "bits",
+    "request_latency_summary": "ms",
+}
