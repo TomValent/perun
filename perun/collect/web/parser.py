88b07e1b280df617d47f94c4972536c65b3973de
Tomas Valent <xvalen27@fit.vutbr.cz>
2024-03-12 22:03:12 +0100

    uid

--- perun/collect/web/parser.py
+++ perun/collect/web/parser.py
@@ -5,8 +5,9 @@ import string
 class Parser:
     """ Parser for OpenTelemetry logs"""
 
-    def parse_metric(self, line: string, order: int) -> tuple[int, string, float]:
+    def parse_metric(self, line: string) -> tuple[string, float, string]:
         """Parse metric from log for profile"""
+
         data = json.loads(line)
         current = data['aggregator']['_current']
 
@@ -15,47 +16,10 @@ class Parser:
         else:
             value = current
 
-        return order, data["descriptor"]["name"], value
+        return data["descriptor"]["name"], value, data["labels"].get("route", "")
 
     def parse_trace(self, line: string):
         """Parse trace from log for profile"""
+
         data_dict = json.loads(line)
         pass
-
-# {
-#   "descriptor": {
-#     "name": "request_latency_summary",
-#     "description": "Latency of requests in milliseconds",
-#     "unit": "ms",
-#     "metricKind": 2,
-#     "valueType": 1
-#   },
-#   "labels": {
-#     "route": "/"
-#   },
-#   "aggregator": {
-#     "kind": 2,
-#     "_boundaries": [null],
-#     "_current": {
-#       "buckets": {
-#         "boundaries": [null],
-#         "counts": [1, 0]
-#       },
-#       "sum": 18.845782,
-#       "count": 1
-#     },
-#     "_lastUpdateTime": [1710015898, 591803882]
-#   },
-#   "aggregationTemporality": 2,
-#   "resource": {
-#     "attributes": {
-#       "service.name": "unknown_service:/usr/local/nodejs/bin/node",
-#       "telemetry.sdk.language": "nodejs",
-#       "telemetry.sdk.name": "opentelemetry",
-#       "telemetry.sdk.version": "0.24.0"
-#     }
-#   },
-#   "instrumentationLibrary": {
-#     "name": "OpenTelemetry-metrics"
-#   }
-# }
